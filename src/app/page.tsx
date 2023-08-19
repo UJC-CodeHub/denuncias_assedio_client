5714a1048f6a2905bec57af4eb8344d630c9524a
LissaiDev <lissaidev@outlook.com>
2023-08-19 13:38:41 +0200

    INIT: Created project structure

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex justify-center items-center bg-slate-800 text-white">
+      <div>
+        <h1 className="text-5xl font-bold text-center">Olá</h1>
+        <h2 className="text-2xl">Seja bem-vindo ao seu ambiente de desenvolvimento</h2>
+      </div>
+    </main>
+  );
+}
