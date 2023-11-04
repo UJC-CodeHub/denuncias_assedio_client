f64936b8eb40e65577285e31f442b5832ff5d028
LissaiDev <lissadev@outlook.com>
2023-11-04 14:28:54 +0200

    feat: create register page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import HomeContainer from "@/containers/Home";
+import LoginContainer from "@/containers/Login";
 
 export default function Home() {
-  return <HomeContainer />
+  return <LoginContainer />
 }
