4c4f17cfba55b5f588ddd274ccca14d0b7ee32b0
LissaiDev <lissadev@outlook.com>
2023-11-04 14:43:03 +0200

    fix: layout issues
    - Layout changes from page to page so it should not be persisted across them

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
+import Footer from "@/components/layout/Footer";
+import Header from "@/components/layout/Header";
 import HomeContainer from "@/containers/Home";
 import LoginContainer from "@/containers/Login";
 
 export default function Home() {
-  return <LoginContainer />
+  return (
+    <>
+      <Header />
+      <HomeContainer />
+      <Footer />
+    </>
+  );
 }
